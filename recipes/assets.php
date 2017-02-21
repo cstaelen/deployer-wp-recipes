79c7b4f32a023fd90cd1e6db521f71900f8ecbb0
cstaelen <clement@ukhom.com>
2017-02-21 10:46:21 +0100

    Optional gulp theme build command && avoid rsync to chown files

--- recipes/assets.php
+++ recipes/assets.php
@@ -9,9 +9,11 @@ task('deploy:assets', function() {
 
     $config = get('wp-recipes');
 
-    writeln('<comment>> Compile assets locally ...</comment>');
-    writeln($config['gulp_cmd']);
-    runLocally('cd ' . $config['theme_dir'] . $config['theme_name'] . ' && ' . $config['gulp_cmd']);
+    if (!empty($config['gulp_cmd'])) {
+        writeln('<comment>> Compile assets locally ...</comment>');
+        writeln($config['gulp_cmd']);
+        runLocally('cd ' . $config['theme_dir'] . $config['theme_name'] . ' && ' . $config['gulp_cmd']);
+    }
     writeln('<comment>> Uploads assets ...</comment>');
     run('rm  -rf ' . $config['theme_dir'] . $config['theme_name']);
     upload( $config['theme_dir'] . $config['theme_dist'], '{{release_path}}/' . $config['theme_dir'] . $config['theme_name']);
