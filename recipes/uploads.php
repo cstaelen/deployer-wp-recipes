4f4dbdd26c3fff3f82a221f48fb43cb5f8408338
Daniel Roe <daniel@concision.co.uk>
2017-10-24 10:33:33 +0100

    Fixed typo

--- recipes/uploads.php
+++ recipes/uploads.php
@@ -10,9 +10,9 @@ task('uploads:sync', function() {
     $rsync_options = '-avzO --no-o --no-g -e';
 
     writeln('<comment>> Receive remote uploads ... </comment>');
-    download('{{deploy_path}}/shared/$upload_dir/', $upload_dir, [ $rsync_options ]);
+    download('{{deploy_path}}/shared/' .$upload_dir . '/', $upload_dir, [ $rsync_options ]);
 
     writeln('<comment>> Send local uploads ... </comment>');
-    upload($upload_dir . '/', '{{deploy_path}}/shared/$upload_dir', [ $rsync_options ]);
+    upload($upload_dir . '/', '{{deploy_path}}/shared/' . $upload_dir, [ $rsync_options ]);
 
 })->desc('Sync uploads');
