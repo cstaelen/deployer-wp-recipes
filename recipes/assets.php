8959c83bbfc051aad65b7975b307610c1ffe1a1f
Daniel Roe <daniel@concision.co.uk>
2017-12-20 15:05:15 +0000

    uploads:sync -> pull and push. Assets now part of deploy.

--- recipes/assets.php
+++ recipes/assets.php
@@ -9,13 +9,11 @@ task('deploy:assets', function() {
 
     $config = get('wp-recipes');
 
-    if (!empty($config['gulp_cmd'])) {
-        writeln('<comment>> Compile assets locally ...</comment>');
-        writeln($config['gulp_cmd']);
-        runLocally('cd ' . $config['theme_dir'] . $config['theme_name'] . ' && ' . $config['gulp_cmd']);
-    }
+    writeln('<comment>> Run composer install in local directory ...</comment>');
+    runLocally('cd ' . $config['theme_dir'] . $config['theme_name'] . ' && ' . $config['gulp_cmd']);
+
     writeln('<comment>> Upload assets ...</comment>');
     run('rm  -rf ' . $config['theme_dir'] . $config['theme_name']);
-    upload( $config['assets_dist'], '{{deploy_path}}/' . $config['assets_dist']);
+    upload( $config['assets_dist'], '{{release_path}}/' . $config['assets_dist']);
 
 })->desc('Upload dist assets folder');
--- recipes/uploads.php
+++ recipes/uploads.php
@@ -5,19 +5,6 @@
 
 namespace Deployer;
 
-task('uploads:sync', function() {
-    $upload_dir = 'web/app/uploads';
-    $rsync_options = '-avzO --no-o --no-g -e';
-
-    writeln('<comment>> Receive remote uploads ... </comment>');
-    download('{{deploy_path}}/shared/' .$upload_dir . '/', $upload_dir, [ $rsync_options ]);
-
-    writeln('<comment>> Send local uploads ... </comment>');
-    upload($upload_dir . '/', '{{deploy_path}}/shared/' . $upload_dir, [ $rsync_options ]);
-
-})->desc('Sync uploads');
-
-
 task('uploads:push', function() {
     $upload_dir = 'web/app/uploads';
     $rsync_options = '-avzO --no-o --no-g -e --delete';
@@ -26,4 +13,13 @@ task('uploads:push', function() {
     upload($upload_dir . '/', '{{deploy_path}}/shared/' . $upload_dir, [ $rsync_options ]);
 
 })->desc('Sync uploads');
- 
+
+
+task('uploads:pull', function() {
+    $upload_dir = 'web/app/uploads';
+    $rsync_options = '-avzO --no-o --no-g -e --delete';
+
+    writeln('<comment>> Receive remote uploads ... </comment>');
+    download('{{deploy_path}}/shared/' . $upload_dir . '/', $upload_dir, [ $rsync_options ]);
+
+})->desc('Sync uploads');
